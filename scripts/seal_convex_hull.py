f910d6eba78b2bdf991f2cb638131c070cf95711
Ehsan Latif <ehsanlatif000@gmail.com>
2023-04-29 02:23:36 -0400

    readme updated with latest scripts

--- scripts/seal_convex_hull.py
+++ scripts/seal_convex_hull.py
@@ -4,225 +4,76 @@ __author__ = 'Ehsan Latif'
 __version__ = '1.0'
 
 
-import math
-from turtle import circle
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
-from matplotlib import pyplot as pb
-import random
-from datetime import datetime
-import time
-import g2o 
-import os
-
-import argparse
-from nav_msgs.msg import Odometry
-
+#!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import String
-from geometry_msgs.msg import PoseStamped
-from robot_msgs.msg import Robot_Pos, StringList
-from robot_msgs.srv import GetCharger, GetChargerResponse, ReleaseCharger, ReleaseChargerResponse 
+import numpy as np
+from scipy.spatial import ConvexHull
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import PolygonStamped, Point32
+from std_msgs.msg import Float64
 
-areaSize=(10, 10)
-No_Of_Robots = 3
+class ConvexHullNode:
+    def __init__(self):
+        rospy.init_node('convex_hull_node', anonymous=True)
+        self.convex_hull_pub = rospy.Publisher('/convex_hull', PolygonStamped, queue_size=10)
+        self.gp_sub = rospy.Subscriber('/gp_value', Float64, self.gp_callback, queue_size=1)
+        self.odom_sub = rospy.Subscriber('/odom', Odometry, self.odom_callback, queue_size=1)
+        self.scan_sub = rospy.Subscriber('/scan', LaserScan, self.scan_callback, queue_size=1)
 
-total_overall_rss=[]
-total_original_tragectory=[]
+        self.current_gp_value = None
+        self.current_odom = None
+        self.current_scan = None
+        self.rate = rospy.Rate(10)
 
-colors = ['b','r','c','y','k','m', 'g', 'b','r','c','b','g','c','y','k','m', 'g', 'b','r','c']
+    def gp_callback(self, msg):
+        self.current_gp_value = msg.data
 
-def dist(pos1, pos2):
-    return math.sqrt((pos1[0]-pos2[0])**2 + (pos1[1]-pos2[1])**2)
+    def odom_callback(self, msg):
+        self.current_odom = msg.pose.pose.position
 
-initial_poses = {"tb3_0":(0.5,1.7),"tb3_1":(0.5,2.5),"tb3_2":(0.5,3.3)}
-colors = {"tb3_0":'b',"tb3_1":'r',"tb3_2":'c'} #,'y','k','m', 'g', 'b','r','c','b','g','c','y','k','m', 'g', 'b','r','c']
+    def scan_callback(self, msg):
+        self.current_scan = msg.ranges
 
+    def create_convex_hull(self):
+        if self.current_odom is None or self.current_scan is None:
+            return
 
-# axis[0].set_title("Trajectory",fontsize=24)
-# # manager = plt.get_current_fig_manager()
-# # manager.full_screen_toggle()
+        points = []
+        for i, r in enumerate(self.current_scan):
+            angle = i * np.pi / len(self.current_scan)
+            x = self.current_odom.x + r * np.cos(angle)
+            y = self.current_odom.y + r * np.sin(angle)
+            points.append([x, y])
 
-# axis[0].set_ylim(0,areaSize[1])
-# axis[0].set_xlim(0,areaSize[0])
+        points = np.array(points)
+        hull = ConvexHull(points)
+        return points[hull.vertices]
 
-# axis[0].set_xlabel('X-axis', fontsize=22)
-# axis[0].set_ylabel('Y-axis', fontsize=22) 
+    def publish_convex_hull(self, convex_hull_points):
+        polygon_msg = PolygonStamped()
+        polygon_msg.header.stamp = rospy.Time.now()
+        polygon_msg.header.frame_id = "odom"
+        for point in convex_hull_points:
+            point32 = Point32()
+            point32.x = point[0]
+            point32.y = point[1]
+            polygon_msg.polygon.points.append(point32)
 
-# axis[0].tick_params(axis='x', labelsize=20)
-# axis[0].tick_params(axis='y', labelsize=20)
-graph = {}
-step = 0
-figure, axis = plt.subplots(1, 2)
+        self.convex_hull_pub.publish(polygon_msg)
 
-axis[0].cla()
-axis[0].set_title("Trajectory",fontsize=24)
-# manager = plt.get_current_fig_manager()
-# manager.full_screen_toggle()
+    def run(self):
+        while not rospy.is_shutdown():
+            convex_hull_points = self.create_convex_hull()
+            if convex_hull_points is not None:
+                self.publish_convex_hull(convex_hull_points)
 
-axis[0].set_ylim(-10,10)
-axis[0].set_xlim(-10,10)
-
-axis[0].set_xlabel('X-axis', fontsize=18)
-axis[0].set_ylabel('Y-axis', fontsize=18) 
-
-axis[0].tick_params(axis='x', labelsize=16)
-axis[0].tick_params(axis='y', labelsize=16)
-axis[0].set_xticks(np.arange(-10,10, 1))
-axis[0].set_yticks(np.arange(-10,10, 1))
-
-axis[1].cla()
-axis[1].set_title("Graph",fontsize=24)
-plt.axis("equal")
-
-axis[1].set_xlabel('X-axis', fontsize=18)
-axis[1].set_ylabel('Y-axis', fontsize=18) 
-
-axis[1].tick_params(axis='x', labelsize=16)
-axis[1].tick_params(axis='y', labelsize=16)
-axis[1].set_ylim(-11,11)
-axis[1].set_xlim(-10,10)
-axis[1].set_xticks(np.arange(-11,11, 1))
-axis[1].set_yticks(np.arange(-10,10, 1))
-
-mng = plt.get_current_fig_manager()
-mng.resize(*mng.window.maxsize())
-
-now = datetime.now()
-
-robots = ["tb3_0","tb3_1","tb3_2"]
-
-prev_time = now.strftime("%H:%M:%S")
-def position_callback(odom_positions,positions):
-    # print(positions)
-    global graph
-    global step
-    global total_original_tragectory
-    global figure
-    global axis
-    global prev_time
-
-
-    circles = []
-    texts = axis[1].plot()
-    lines =  []
-    
-    current_trajectory = {}
-    for robot_i in robots:
-        
-        # robot_poses[robot_i] = positions[robot_i]
-        robot_i_frame = robot_i
-        # print("robot:"+ robot_i_frame)
-        # print(robot_i_frame)
-        robot_i_position = positions[robot_i]
-        current_trajectory[robot_i+'_pos']=robot_i_position
-        current_trajectory[robot_i+'_odom']=odom_positions[robot_i]
-
-        graph[robot_i_frame]={}
-        for robot_j in robots:
-            robot_j_frame = robot_j
-            robot_j_position = positions[robot_j]
-            if robot_j_frame!=robot_i_frame:    
-                robot_distance = dist(robot_i_position,robot_j_position)
-                if robot_distance < 5:
-                    graph [robot_i_frame][robot_j_frame] = robot_distance
-                circles.append(axis[1].add_artist(plt.Circle((robot_i_position[0],robot_i_position[1] ), radius=0.2, color=colors[robot_i_frame] )))
-                texts.append(axis[1].text(robot_i_position[0]-0.02,robot_i_position[1], str(robot_i_frame.split('_')[-1]), fontsize = 10, color='w'))
-                lines.append(axis[1].plot([robot_i_position[0],robot_j_position[0]],[robot_i_position[1],robot_j_position[1]],colors[robot_i_frame]+'-',linewidth=2,clip_on=False))
-        # print(step)s
-        if step > 0:
-            axis[0].plot([total_original_tragectory[step-1][robot_i+'_odom'][0],current_trajectory[robot_i+'_odom'][0]],[total_original_tragectory[step-1][robot_i+'_odom'][1],current_trajectory[robot_i+'_odom'][1]],colors[robot_i_frame]+'-',linewidth=2,clip_on=False)
-            # axis[0].plot([total_original_tragectory[step-1][robot_i+'_pos'][0],current_trajectory[robot_i+'_pos'][0]],[total_original_tragectory[step-1][robot_i+'_pos'][1],current_trajectory[robot_i+'_pos'][1]],colors[robot_i_frame]+'--',linewidth=2,clip_on=False)
-
-        # else:
-        #     axis[0].plot([total_original_tragectory[ste][robot_i_frame].x,total_original_tragectory[step][robot_i_frame].x],[total_original_tragectory[step-1][robot_i_frame].y,total_original_tragectory[step][robot_i_frame].y],colors[int(robot_i_frame)-3]+'-',linewidth=2,clip_on=False)
-
-    total_original_tragectory.append(current_trajectory)
-    step+=1
-
-
-    plt.draw()
-    plt.pause(0.000001)
-    for line in lines:
-        line.pop(0).remove()
-    for circle in circles:
-        circle.remove()
-    for text in texts:
-        text.remove()
-
-
-
-# plt.show(block=False)
-# plt.pause(3)
-# plt.savefig('Dist_graph_optm_localization.png')
-# plt.clf()
-# plt.close()
-
-def active_robots_callback(robots):
-    global No_Of_Robots
-    No_Of_Robots = len(robots.data)
-
-positions = {"tb3_0":(0,0),"tb3_1":(0,0),"tb3_2":(0,0)}
-odom_positions = {"tb3_0":(0,0),"tb3_1":(0,0),"tb3_2":(0,0)}
-
-def tb3_0_odom_callback(msg):
-    global positions
-    global odom_positions
-    odom_x,odom_y= msg.pose.pose.position.x,msg.pose.pose.position.y
-    odom_positions["tb3_0"]=(odom_x,odom_y)
-    pos_x,pos_y=odom_x+random.uniform(0.01,0.20),odom_y+random.uniform(0.01,0.20)
-    positions["tb3_0"] = (pos_x,pos_y)
-    if odom_positions["tb3_1"]!=(0,0) and odom_positions["tb3_2"]!=(0,0):
-        position_callback(odom_positions,positions)
-
-def tb3_1_odom_callback(msg):
-    global positions
-    global odom_positions
-    odom_x,odom_y= msg.pose.pose.position.x,msg.pose.pose.position.y
-    odom_positions["tb3_1"]=(odom_x,odom_y)
-    pos_x,pos_y=odom_x+random.uniform(0.01,0.20),odom_y+random.uniform(0.01,0.20)
-    positions["tb3_1"] = (pos_x,pos_y)
-    if odom_positions["tb3_0"]!=(0,0) and odom_positions["tb3_2"]!=(0,0):
-        position_callback(odom_positions,positions)
-
-
-def tb3_2_odom_callback(msg):
-    global positions
-    global odom_positions
-    odom_x,odom_y= msg.pose.pose.position.x,msg.pose.pose.position.y
-    odom_positions["tb3_2"]=(odom_x,odom_y)
-    pos_x,pos_y=odom_x+random.uniform(0.01,0.20),odom_y+random.uniform(0.01,0.20)
-    positions["tb3_2"] = (pos_x,pos_y)
-    if odom_positions["tb3_1"]!=(0,0) and odom_positions["tb3_0"]!=(0,0):
-        position_callback(odom_positions,positions)
-
-
-
-
+            self.rate.sleep()
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('dgorl', anonymous=True)
-        # interfacename = rospy.get_param('~INTERFACE_NAME', 'wlan0')
-        # update_rate = rospy.get_param('~update_rate_wireless_quality', 10)	
-        pub_position = rospy.Publisher('position', PoseStamped, queue_size=10)
-        rate = rospy.Rate(1)
-
-        # rospy.Subscriber("/positions", Robot_Pos, position_callback,queue_size=1)
-        # rospy.Subscriber("/active_robots", StringList, active_robots_callback,queue_size=1)
-        rospy.Subscriber('/tb3_0/odom',Odometry,tb3_0_odom_callback,queue_size=1)
-        rospy.Subscriber('/tb3_1/odom',Odometry,tb3_1_odom_callback,queue_size=1)
-        rospy.Subscriber('/tb3_2/odom',Odometry,tb3_2_odom_callback,queue_size=1)
-
-
-
-        plt.ion()
-
-        plt.show(block=True)
-        # spin() simply keeps python from exiting until this node is stopped
-        rospy.spin()
+        convex_hull_node = ConvexHullNode()
+        convex_hull_node.run()
     except rospy.ROSInterruptException:
         pass
